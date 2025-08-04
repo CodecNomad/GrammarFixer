3dc37a44b47eef8082ef9af9b08e8fba90263f2b
codecnomad <codecnomad@github.com>
2025-08-04 10:22:38 +0200

    Added inline options for customizability

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
+	"strings"
 
 	"google.golang.org/genai"
 )
@@ -13,40 +15,70 @@ import (
 func main() {
 	apiKey := os.Getenv("GEMINI_APIKEY")
 	if apiKey == "" {
+		sendNotification("Invalid API key.", NotificationLevelError)
 		log.Fatal("Invalid API key. Please set the GEMINI_APIKEY environment variable.")
-		sendNotification("[ERROR] GrammarFixer", "Invalid API key.")
 	}
 
 	ctx := context.Background()
 	client, err := genai.NewClient(ctx, &genai.ClientConfig{APIKey: apiKey})
 	if err != nil {
+		sendNotification("Unable to create GenAI client.", NotificationLevelError)
 		log.Fatalf("Failed to create GenAI client: %v", err)
-		sendNotification("[ERROR] GrammarFixer", "Unable to create GenAI client.")
 	}
 
-	sendNotification("[INFO] GrammarFixer", "Started fixing grammar")
+	sendNotification("Started", NotificationLevelInfo)
 
 	clipboardText, err := getClipboardText()
 	if err != nil {
+		sendNotification("Unable to read clipboard.", NotificationLevelError)
 		log.Fatalf("Failed to read from clipboard: %v", err)
-		sendNotification("[ERROR] GrammarFixer", "Unable to read clipboard.")
 	}
 
 	correctedText, err := fixGrammar(ctx, client, clipboardText)
 	if err != nil {
+		sendNotification("Unable to process text.", NotificationLevelError)
 		log.Fatalf("Failed to process text: %v", err)
-		sendNotification("[ERROR] GrammarFixer", "Unable to process text.")
 	}
 
 	if err := writeToClipboard(correctedText); err != nil {
+		sendNotification("Unable to write text to clipboard.", NotificationLevelError)
 		log.Fatalf("Failed to write to clipboard: %v", err)
-		sendNotification("[ERROR] GrammarFixer", "Unable to write text to clipboard.")
+
 	}
 
-	sendNotification("[INFO] GrammarFixer", "Finished fixing grammar")
+	summary := correctedText
+	if len(correctedText) > 50 {
+		summary = correctedText[:50] + "..."
+	}
+	sendNotification("Finished: "+summary, NotificationLevelInfo)
 }
 
-func sendNotification(title string, text string) error {
+const (
+	NotificationLevelError = 2
+	NotificationLevelWarn  = 1
+	NotificationLevelInfo  = 0
+)
+
+func sendNotification(text string, level int) error {
+	var title string
+	switch level {
+	case NotificationLevelError:
+		{
+			title = "[ERROR]"
+		}
+
+	case NotificationLevelWarn:
+		{
+			title = "[WARNING]"
+		}
+
+	case NotificationLevelInfo:
+		{
+			title = "[INFO]"
+		}
+	}
+	title += " GrammarFixer"
+
 	cmd := exec.Command("notify-send", "-t", "1500", title, text)
 	err := cmd.Run()
 	return err
@@ -67,16 +99,45 @@ func writeToClipboard(text string) error {
 }
 
 func fixGrammar(ctx context.Context, client *genai.Client, input string) (string, error) {
+	model := "gemini-2.5-flash"
+	if strings.Contains(input, "[QUALITY]") {
+		model = "gemini-2.5-pro"
+		input = strings.ReplaceAll(input, "[QUALITY]", "")
+	}
+
+	if strings.Contains(input, "[FAST]") {
+		model = "gemini-2.5-flash-lite"
+		input = strings.ReplaceAll(input, "[QUALITY]", "")
+	}
+
+	re := regexp.MustCompile(`(?i)\[INST=(.*?)\]`)
+	matches := re.FindStringSubmatch(input)
+	var customInstruction string
+	if len(matches) > 1 {
+		customInstruction = matches[1]
+		input = strings.ReplaceAll(input, matches[0], "")
+		customInstruction = " " + customInstruction
+	}
+
+	re = regexp.MustCompile(`(?i)\[STYLE=(.*?)\]`)
+	matches = re.FindStringSubmatch(input)
+	var customStyle string
+	if len(matches) > 1 {
+		customStyle = matches[1]
+		input = strings.ReplaceAll(input, matches[0], "")
+		customStyle = " Using this style description as a guide: ```" + customStyle + "```"
+	}
+
 	resp, err := client.Models.GenerateContent(
 		ctx,
-		"gemini-2.5-flash-lite",
+		model,
 		genai.Text(input),
 		&genai.GenerateContentConfig{
 			SystemInstruction: &genai.Content{
 				Role: "system",
 				Parts: []*genai.Part{
 					{
-						Text: "Fix the grammar and make it sound natural and human. Only send back the corrected version without explanations. Keep the original wording as much as possible. If the original message is rude, keep that tone. Make sure not to use em dashes. Make sure the original message's meaning stays the same. Use the same exact language as the original language was in.",
+						Text: "Fix the grammar and make it sound natural and human. Only send back the corrected version without explanations. Keep the original wording as much as possible. If the original message is rude, keep that tone. Make sure not to use em dashes. Make sure the original message's meaning stays the same. Use the same exact language as the original language was in." + customStyle + customInstruction,
 					},
 				},
 			},
